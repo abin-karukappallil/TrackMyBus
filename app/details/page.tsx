7acd246de2eaafd726996653222deabeeeb4b779
Abin Karukappallil <followgamer102@gmail.com>
2024-08-27 18:01:25 +0530

    data table

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -47,16 +47,20 @@ const Details = ()=>  {
       }
     
   return (
-    <div className=" min-h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="gap-4 flex flex-col justify-center items-center max-w-2xl mx-auto p-4">
-    <div className="overflow-y-hidden">
+    <div className="min-h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="h-[80vh] gap-4 flex flex-col justify-center items-center w-[100vh] mx-auto p-4">
+    <div className="overflow-y-scroll z-10">
        {data.map((trip)=>(
-       
-            <div className="overflow-y-hidden" key={trip.trip}>
+            <div  key={trip.trip}>
                 
-                <p className="text-yellow-400">Trip No: {trip.trip}</p>
+                <div className="text-yellow-400 w-full">
+                    <h2>
+                    Trip No: {trip.trip}</h2>
+                    <h4>Bus Number: {value} </h4>
+                    </div>
+
                     {trip.stations.map((station:any,i:any)=>(
-                        <div  key={i} className='overflow-y-hidden bg-yellow-300'>
+                        <div key={i} className='w-full bg-yellow-300'>
                         <p>Station: {station.station}</p>
                         <p>Arrival Time: {station.arrivalTime}</p>
                         <p>depart time: {station.departureTime}</p>
