1312f8215c79503bed6c55db23205dda1f8c4ef4
Abin Karukappallil <followgamer102@gmail.com>
2024-08-27 16:02:44 +0530

    data displayed

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -1,45 +1,67 @@
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react'
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+interface Station {
+    station: string;
+    arrivalTime: string;
+    departureTime: string;
+  }
+
+interface Trip {
+    trip: number;
+    stations: Station[];
+  }
+
 
 const Details = ()=>  {
        const router = useRouter();
+       const [loading, setLoading] = useState<boolean>(true);
        const[value,setvalue] = useState<string>('');
        const [data, setData] = useState<any[]>([]); 
-       useEffect(()=>{
-        if(router.isReady){
-            const busNumber = router.query.value as string;            if(busNumber){
-                if(busNumber){
-                    setvalue(busNumber)
-                    fetchData(busNumber);
-                }
-            }
+       useEffect(() => {
+        const url = new URL(window.location.href);
+        const busNumber = url.searchParams.get('value');
+    
+        if (busNumber) {
+          setvalue(busNumber);
+          fetchData(busNumber);
         }
-       })
+      }, [router]);
+    
 
-
-
-    async function fetchData(busNumber:string){
-        const res = await fetch(`https://busapi.amithv.xyz/api/v1/search?vehicle_number=${busNumber}`);
-        if(!res.ok){
+      async function fetchData(busNumber: string) {
+        setLoading(true); 
+        try {
+          const res = await fetch(`https://busapi.amithv.xyz/api/v1/search?vehicle_number=${busNumber}`);
+          if (!res.ok) {
             throw new Error('Fetching failed');
+          }
+          const result: Trip[] = await res.json();
+          setData(result); 
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        } finally {
+          setLoading(false);
         }
-        const result =  await res.json();
-        setData(result.trip);
-        
-    }
-   
+      }
+    
+      if (loading) {
+        return <p>Loading...</p>;
+      }
   return (
 
     <div>
        {data.map((trip)=>(
             <div key={trip.trip}>
                 <p>Trip No: {trip.trip}</p>
-                    {trip.station.map((station:any,i:any)=>(
+                    {trip.stations.map((station:any,i:any)=>(
                         <div key={i} className='bg-yellow-300'>
                         <p>Station: {station.station}</p>
+                        <p>Arrival Time: {station.arrivalTime}</p>
+                        <p>depart time: {station.departureTime}</p>
                         </div>
                     ))};
-                <p>arrival</p>
             </div>
        ))};
 
